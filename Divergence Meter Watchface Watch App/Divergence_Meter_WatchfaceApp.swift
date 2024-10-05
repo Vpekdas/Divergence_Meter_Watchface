0e77f9f48ca65aca4a44c5961271a7c46d8ab98d
Vpekdas <volkan.pekdas@hotmail.com>
2024-10-05 18:18:53 +0200

    Initial commit

---
+++ Divergence Meter Watchface Watch App/Divergence_Meter_WatchfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Divergence_Meter_WatchfaceApp.swift
+//  Divergence Meter Watchface Watch App
+//
+//  Created by Volkan Pekdas on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Divergence_Meter_Watchface_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Divergence Meter Watchface Watch AppTests/Divergence_Meter_Watchface_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Divergence_Meter_Watchface_Watch_AppTests.swift
+//  Divergence Meter Watchface Watch AppTests
+//
+//  Created by Volkan Pekdas on 05/10/2024.
+//
+
+import Testing
+@testable import Divergence_Meter_Watchface_Watch_App
+
+struct Divergence_Meter_Watchface_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
